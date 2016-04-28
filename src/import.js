cd67ceb3cc11977965e63b5d67c8ccd5e6a1a8c9
Samlv9 <Samlv9@live.com>
2016-04-28 09:32:21 +0800

    增加 Base16.encode 方法。

--- src/import.js
+++ src/import.js
@@ -35,62 +35,14 @@
 /// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 /// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 /// THE SOFTWARE.
-import Base64 from "./data/codec/Base64";
+import Base16 from "./data/codec/Base16";
 
 
-function str2buf( s ) {
-    var a = new Uint8Array(s.length);
-    for ( var i = 0; i < s.length; ++i ) {
-        a[i] = s.charCodeAt(i);
-    }   
-    return a;
+var test = new Uint8Array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]);
+var bs16 = new Base16(false);
+
+function buf2str( buffer ) {
+    return String.fromCharCode.apply(String, buffer);   
 }
 
-function buf2str( a ) {
-    return String.fromCharCode.apply(String, a);   
-}
-
-
-var b64 = new Base64();
-var str = 'Man is distinguished, not only by his reason, but by this singular passion'   + '' +
-          ' from other animals, which is a lust of the mind, that by a perseverance of'  + '' +
-          ' delight in the continued and indefatigable generation of knowledge, exceeds' + '' +
-          ' the short vehemence of any carnal pleasure.';
-var dat = 'TWFuIGlzIGRpc3Rpbmd1aXNoZWQsIG5vdCBvbmx5IGJ5IGhpcyByZWFzb24sIGJ1dCBieSB0aGlz' +
-          'IHNpbmd1bGFyIHBhc3Npb24gZnJvbSBvdGhlciBhbmltYWxzLCB3aGljaCBpcyBhIGx1c3Qgb2Yg' +
-          'dGhlIG1pbmQsIHRoYXQgYnkgYSBwZXJzZXZlcmFuY2Ugb2YgZGVsaWdodCBpbiB0aGUgY29udGlu' +
-          'dWVkIGFuZCBpbmRlZmF0aWdhYmxlIGdlbmVyYXRpb24gb2Yga25vd2xlZGdlLCBleGNlZWRzIHRo' +
-          'ZSBzaG9ydCB2ZWhlbWVuY2Ugb2YgYW55IGNhcm5hbCBwbGVhc3VyZS4=';
-          
-          
-          
-console.time("label");
-var buffers = str.split("");
-
-for ( var i = 0; i < buffers.length; ++i ) {
-    buffers[i] = str2buf(buffers[i]);
-    // console.log("buffer-length:", buffers[i].length, buffers[i].length % 3);   
-}
-
-console.log("length:", buffers.length, buffers.length % 3);
-
-var results = [];
-
-for ( var i = 0; i < buffers.length; ++i ) {
-    results[i] = b64.encode(buffers[i], i == buffers.length - 1);
-}
-
-for ( var i = 0; i < results.length; ++i ) {
-    results[i] = buf2str(results[i]);
-}
-
-var enstr = results.join("");
-var enbuf = str2buf(enstr);
-var debuf = b64.decode(enbuf, true);
-var destr = buf2str(debuf);
-
-console.log(dat);
-console.log(destr);
-console.log("Encode:", dat == enstr);
-console.log("Decode:", str == destr);
-console.timeEnd("label");
+console.log("result:", buf2str(bs16.encode(test)) );
