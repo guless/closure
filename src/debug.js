91bdc0f9cb28fa0100645188c3da33cf1e49518f
Samlv9 <Samlv9@live.com>
2016-05-25 11:15:07 +0800

    实现 MD4 消息摘要函数。

--- src/debug.js
+++ src/debug.js
@@ -38,6 +38,7 @@
 import tobytes from "./data/tobytes";
 import tochars from "./data/tochars";
 import MD5 from "./data/crypto/MD5";
+import MD4 from "./data/crypto/MD4";
 import Base16 from "./data/codec/Base16";
 
 
@@ -49,6 +50,16 @@ function test( str, dat ) {
     console.log("dat:", dat);
     console.log("md5:", md5);
     console.log("equal:", md5 == dat);
+}
+
+function test_md4( str, dat ) {
+    var b16 = new Base16();
+    var md4 = tochars(b16.encode((new MD4()).update(tobytes(str,1)).digest()));
+    
+    console.log("str:", str);
+    console.log("dat:", dat);
+    console.log("md4:", md4);
+    console.log("equal:", md4 == dat);
 }
 
 // MD5 test suite:
@@ -68,3 +79,23 @@ test("message digest", "f96b697d7cb7938d525a2f31aaf161d0");
 test("abcdefghijklmnopqrstuvwxyz", "c3fcd3d76192e4007dfb496cca67e13b");
 test("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789", "d174ab98d277d9f5a5611c2c9f419d9f");
 test("12345678901234567890123456789012345678901234567890123456789012345678901234567890", "57edf4a22be3c955ac49da2e2107b67a");
+
+console.log("Test-MD4:-------------------->");
+
+// MD4 test suite:
+// MD4 ("") = 31d6cfe0d16ae931b73c59d7e0c089c0
+// MD4 ("a") = bde52cb31de33e46245e05fbdbd6fb24
+// MD4 ("abc") = a448017aaf21d8525fc10ae87aa6729d
+// MD4 ("message digest") = d9130a8164549fe818874806e1c7014b
+// MD4 ("abcdefghijklmnopqrstuvwxyz") = d79e1c308aa5bbcdeea8ed63df412da9
+// MD4 ("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789") =
+// 043f8582f241db351ce627e153e7f0e4
+// MD4 ("12345678901234567890123456789012345678901234567890123456789012345678901234567890") = e33b4ddc9c38f2199c3e7b164fcc0536
+
+test_md4("", "31d6cfe0d16ae931b73c59d7e0c089c0");
+test_md4("a", "bde52cb31de33e46245e05fbdbd6fb24");
+test_md4("abc", "a448017aaf21d8525fc10ae87aa6729d");
+test_md4("message digest", "d9130a8164549fe818874806e1c7014b");
+test_md4("abcdefghijklmnopqrstuvwxyz", "d79e1c308aa5bbcdeea8ed63df412da9");
+test_md4("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789", "043f8582f241db351ce627e153e7f0e4");
+test_md4("12345678901234567890123456789012345678901234567890123456789012345678901234567890", "e33b4ddc9c38f2199c3e7b164fcc0536");
