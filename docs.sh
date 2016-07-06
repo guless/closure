884da16e417cdc0187fac609fd7435f07c818e64
Samlv9 <Samlv9@live.com>
2016-07-06 18:42:05 +0800

    添加 docs 自动提交工具。

--- docs.sh
+++ docs.sh
@@ -1,2 +1,22 @@
 #!/bin/sh
 ./node_modules/.bin/esdoc -c esdoc.json
+
+if [ ! -d ./tmp ]
+then
+  mkdir ./tmp
+fi
+
+mv ./docs ./tmp
+na=$(git rev-parse --abbrev-ref HEAD)
+git checkout gh-pages
+
+if [ -d ./docs ]
+then
+  rm -r ./docs
+fi
+
+mv ./tmp/docs ./
+git add ./docs
+git commit -m "Update api docs."
+git push
+git checkout $(na)
